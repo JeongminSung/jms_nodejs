06a8b6f9742299f04a1b631c40f3573c826a8c78
JeongminSung <hellojeongmin@gmail.com>
2021-05-30 17:28:11 +0900

    node express 초기세팅

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    indent: ["error", 4],
+    semi: true,
+    "no-trailing-spaces": 0,
+    "keyword-spacing": 0,
+    "no-unused-vars": 1,
+    "no-multiple-empty-lines": 0,
+    "space-before-function-paren": 0,
+    "eol-last": 0,
+  },
+  ignorePatterns: ["node_modules/"],
+};
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # jms_nodejs
+
+## 노드 버전 업데이트 하기
+
+- 기존에 쓰던 노드 버전 : 14.16.1
+- 업데이트 할 노드 버전 : 14.16.0
+
+```
+// 노드 업데이트
+node -v //node 버전 확인
+npm cache clean -f //캐시 삭제
+sudo npm install -g n //nodejs 버전관리 플러그인
+n lts //lts 버전
+// npm 업데이트
+npm -v //npm 버전 확인
+sudo npm i -g npm //npm 업데이트(global 경로)
+// 최종 버전 확인
+node -v
+npm -v
+```
